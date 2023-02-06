abf49c93e365aa99e7313aa0cfbfd6045a35a544
marcelosanchez166 <marcelosanchez166@gmail.com>
2023-02-05 19:23:47 -0600

    Identacion de archivos

--- Cartelera_cine.py
+++ Cartelera_cine.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import font
 from typing import Sized
-import Detalles_Movies 
+import Detalles_Movies
 
 from tkcalendar import Calendar
 
@@ -10,128 +10,127 @@ from tkcalendar import Calendar
 """Las ventanas emegentes no vienen con la biblioteca de tkinter hay que importarlas por aparte"""
 from tkinter import messagebox
 
-raiz=Tk()
-raiz.title(" Cartelera de Cine " )
+raiz = Tk()
+raiz.title(" Cartelera de Cine ")
 raiz.geometry("750x750")
 raiz.config(bg="#A9A9A9")
 
-selected_movie=StringVar()
+selected_movie = StringVar()
 
 
-#---------------------funcion para crear una ventana emergente-----------------
-Descuento=0.10
-Entrada=2.50
+# ---------------------funcion para crear una ventana emergente-----------------
+Descuento = 0.10
+Entrada = 2.50
+
 
 def MostrarDatos():
-          Nclientes=0
-          Nclientes=float(Cantidad_Cliente.get())
-          #diccionario={"peli":pelicula.get(), "cancliente":Cantidad_Cliente.get() }
-          #dato=messagebox.showinfo("Cartelera", diccionario)
-          lista=[]
-          lista.append(pelicula.get())
-          #print(type(Cantidad_Cliente.get()))
-          lista.append(Cantidad_Cliente.get() )
-          frec=lista.append(frecuente.get())
-          Message.insert(END,"\n")
-          Message.insert(END,"-----------------------------")
-          Message.insert(END,"\n")
-          Message.insert(END,"Pelicula: ")
-          Message.insert(END, lista[0]+"\n")#Se le pasa al label la opcion escogida
-          Message.insert(END, "Clientes: " )
-          Message.insert(END, lista[1]+"\n" )
-          Message.insert(END, "Cliente Frecuente: " )
-          Message.insert(END, lista[2]+"\n" )
-          Message.insert(END, "Fecha de funcion: ")
-          Message.insert(END, cal.get_date()+"\n") 
-          Message.insert(END,"Idioma: ")
-          Message.insert(END,SelecIdioma.get()+ "\n")
-          
-          if frecuente.get()=="Si":
-                    Message.insert(END,"Se aplicara descuento del 10% "+"\n" )
-                    #print(type(Nclientes))
-                    desc=Descuento*Entrada
-                    Preciototal=Entrada*Nclientes
-                    total=Preciototal-desc
-                    Message.insert(END, "Total a pagar: ")
-                    Message.insert(END,total ,"\n")
-          else:
-                    Message.insert(END,"No se aplicara descuentos "+"\n")
-                    Preciototal=Entrada*Nclientes
-                    Message.insert(END, "Total a pagar: ")
-                    Message.insert(END,Preciototal , " \n ")
-          
-          
+    Nclientes = 0
+    Nclientes = float(Cantidad_Cliente.get())
+    #diccionario={"peli":pelicula.get(), "cancliente":Cantidad_Cliente.get() }
+    #dato=messagebox.showinfo("Cartelera", diccionario)
+    lista = []
+    lista.append(pelicula.get())
+    # print(type(Cantidad_Cliente.get()))
+    lista.append(Cantidad_Cliente.get())
+    frec = lista.append(frecuente.get())
+    Message.insert(END, "\n")
+    Message.insert(END, "-----------------------------")
+    Message.insert(END, "\n")
+    Message.insert(END, "Pelicula: ")
+    # Se le pasa al label la opcion escogida
+    Message.insert(END, lista[0]+"\n")
+    Message.insert(END, "Clientes: ")
+    Message.insert(END, lista[1]+"\n")
+    Message.insert(END, "Cliente Frecuente: ")
+    Message.insert(END, lista[2]+"\n")
+    Message.insert(END, "Fecha de funcion: ")
+    Message.insert(END, cal.get_date()+"\n")
+    Message.insert(END, "Idioma: ")
+    Message.insert(END, SelecIdioma.get() + "\n")
 
-                    
+    if frecuente.get() == "Si":
+        Message.insert(END, "Se aplicara descuento del 10% "+"\n")
+        # print(type(Nclientes))
+        desc = Descuento*Entrada
+        Preciototal = Entrada*Nclientes
+        total = Preciototal-desc
+        Message.insert(END, "Total a pagar: ")
+        Message.insert(END, total, "\n")
+    else:
+        Message.insert(END, "No se aplicara descuentos "+"\n")
+        Preciototal = Entrada*Nclientes
+        Message.insert(END, "Total a pagar: ")
+        Message.insert(END, Preciototal, " \n ")
 
 
-
-Message=Text(raiz,width=50, height=10)#Label para mostrar lo que el usuario ha seleccionado
+# Label para mostrar lo que el usuario ha seleccionado
+Message = Text(raiz, width=50, height=10)
 Message.grid(row=9, column=0, padx=10, columnspan=10)
 
 
-
-Personas=Label(raiz,text="Personas ", padx=29)
+Personas = Label(raiz, text="Personas ", padx=29)
 Personas.grid(row=0, column=0)
-Personas.config(bg="#8A2BE2",fg="#000000", font="italic")
-Cantidad_Cliente=ttk.Combobox(raiz)
-Cantidad_Cliente["values"]=[1,2,3,4,5,6,7,8,9,10]
+Personas.config(bg="#8A2BE2", fg="#000000", font="italic")
+Cantidad_Cliente = ttk.Combobox(raiz)
+Cantidad_Cliente["values"] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 Cantidad_Cliente.current(0)
-Cantidad_Cliente.grid(row=0,column=1)
+Cantidad_Cliente.grid(row=0, column=1)
 
 
-PeliLabel=Label(raiz,text="Peliculas ", padx=31)
+PeliLabel = Label(raiz, text="Peliculas ", padx=31)
 PeliLabel.grid(row=3, column=0)
-PeliLabel.config(bg="#8A2BE2",fg="#000000", font="italic")
-pelicula=ttk.Combobox(raiz, textvariable=selected_movie)
-pelicula["values"]=["Harry potter", "Donde estan las rubias", "Jurasic World"]
-pelicula.current(0)#sirve para que aparezca una opcion por defecto en el combobox, se le pone la posicion del valor que quiero que salga
-pelicula.grid(row=3,column=1)
+PeliLabel.config(bg="#8A2BE2", fg="#000000", font="italic")
+pelicula = ttk.Combobox(raiz, textvariable=selected_movie)
+pelicula["values"] = ["Harry potter",
+                      "Donde estan las rubias", "Jurasic World"]
+# sirve para que aparezca una opcion por defecto en el combobox, se le pone la posicion del valor que quiero que salga
+pelicula.current(0)
+pelicula.grid(row=3, column=1)
 
 
-Clientefrecuente=Label(raiz,text="Cliente Frecuente ")
-Clientefrecuente.config(bg="#8A2BE2",fg="#000000", font="italic")
+Clientefrecuente = Label(raiz, text="Cliente Frecuente ")
+Clientefrecuente.config(bg="#8A2BE2", fg="#000000", font="italic")
 Clientefrecuente.grid(row=6, column=0)
-frecuente=ttk.Combobox(raiz )
-frecuente["values"]=["Si", "No"]
+frecuente = ttk.Combobox(raiz)
+frecuente["values"] = ["Si", "No"]
 frecuente.current(0)
-frecuente.grid(row=6,column=1)
+frecuente.grid(row=6, column=1)
 
 
-#------------------------Seleccion de Idioma----------------------------------
-IdiomaLB=Label(raiz,text="Idioma ",padx=39)
-IdiomaLB.config(bg="#8A2BE2",fg="#000000", font="italic")
+# ------------------------Seleccion de Idioma----------------------------------
+IdiomaLB = Label(raiz, text="Idioma ", padx=39)
+IdiomaLB.config(bg="#8A2BE2", fg="#000000", font="italic")
 IdiomaLB.grid(row=7, column=0)
-SelecIdioma=ttk.Combobox(raiz )
-SelecIdioma["values"]=["Español DOB", "English", "Eng sub Español"]
+SelecIdioma = ttk.Combobox(raiz)
+SelecIdioma["values"] = ["Español DOB", "English", "Eng sub Español"]
 SelecIdioma.current(0)
-SelecIdioma.grid(row=7,column=1)
+SelecIdioma.grid(row=7, column=1)
 
 
+# ------------------Calendario Horario------------------------------------------------------
 
-#------------------Calendario Horario------------------------------------------------------
-
-cal = Calendar(raiz, selectmode = 'day', year = 2021, month = 5, day = 22) 
+cal = Calendar(raiz, selectmode='day', year=2021, month=5, day=22)
 cal.grid(row=9, column=10, pady=5)
 
 
-#--------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------
 
 
-
-Enviarboton=Button(raiz,width=10, text="Mostrar", command=MostrarDatos)#crea un boton con texto dentro y se le pasa la funcion boton1
+# crea un boton con texto dentro y se le pasa la funcion boton1
+Enviarboton = Button(raiz, width=10, text="Mostrar", command=MostrarDatos)
 Enviarboton.config(bg="#008B8B", fg="#191970", font="cursiva")
 Enviarboton.grid(row=12, column=0, pady=10)
 
 
-#-----------------------------Boton y funcion borrar contenido de un cuadro grande de texto-------------
+# -----------------------------Boton y funcion borrar contenido de un cuadro grande de texto-------------
 def Borrar():
-          Message.delete(1.0, END)
+    Message.delete(1.0, END)
 
-botondelete=Button(raiz,text="Eliminar Datos",width=12,command=lambda:Borrar())
+
+botondelete = Button(raiz, text="Eliminar Datos",
+                     width=12, command=lambda: Borrar())
 botondelete.config(bg="#008B8B", fg="#191970", font="cursiva")
-botondelete.grid(row=12,column=1,columnspan=8)
-
+botondelete.grid(row=12, column=1, columnspan=8)
 
 
 raiz.mainloop()
--- MasVentanasEmergentes.py
+++ MasVentanasEmergentes.py
@@ -7,46 +7,47 @@ from tkinter import messagebox
 from tkinter import colorchooser
 from tkinter import filedialog
 
-raiz=Tk()
+raiz = Tk()
 raiz.title("Datos del Usuario ")
 raiz.geometry("720x650")
 raiz.config(background="gray")
 
 
-
 def Abrir():
-          #el parametro askopenfilename devuelve la ruta del archivo cuando le damos abrir
-          valor=filedialog.askopenfilename(title="Abrir archivo ")
-          print(valor)
+    # el parametro askopenfilename devuelve la ruta del archivo cuando le damos abrir
+    valor = filedialog.askopenfilename(title="Abrir archivo ")
+    print(valor)
 
 
-#--------------------------------Funcion para guardar archivo-------------------
+# --------------------------------Funcion para guardar archivo-------------------
 def guardar():
-#El parametro filetypes sirve para decir que tipo de ficheros quiero que muestre se le pasan dos valores la descripcion y extencion
-#El Parametro asksaveasfilename devuelve la ruta donde se guardo el archivo
-          valor2=filedialog.asksaveasfilename(title="Guardar Archivo ", initialdir="D:", filetypes=(("Ficheros de excel ", "*.xlsx"), 
-          ("ficheros de texto", "*.txt"), ("Todos los ficheros ", "*.*")))
-          print(valor2)
+    # El parametro filetypes sirve para decir que tipo de ficheros quiero que muestre se le pasan dos valores la descripcion y extencion
+    # El Parametro asksaveasfilename devuelve la ruta donde se guardo el archivo
+    valor2 = filedialog.asksaveasfilename(title="Guardar Archivo ", initialdir="D:", filetypes=(("Ficheros de excel ", "*.xlsx"),
+    ("ficheros de texto", "*.txt"), ("Todos los ficheros ", "*.*")))
+    print(valor2)
 
 
-#------------Funcion para guardar un archivo como---------------
+# ------------Funcion para guardar un archivo como---------------
 def guardar_como():
-          #El initial le dice la ruta que estableci para que se guarde por defecto los archivos
-          #El Parametro asksaveasfile devuelve la ruta donde se guardo el archivo
-          valor3=filedialog.asksaveasfile(title="Guardar Archivo ", initialdir="D:\Docker BO")
-          print(valor3)
+    # El initial le dice la ruta que estableci para que se guarde por defecto los archivos
+    # El Parametro asksaveasfile devuelve la ruta donde se guardo el archivo
+    valor3 = filedialog.asksaveasfile(
+        title="Guardar Archivo ", initialdir="D:\Docker BO")
+    print(valor3)
 
 
-#-----------------------Crear menu de opciones ---------------------------------
-barramenu=Menu(raiz)
+# -----------------------Crear menu de opciones ---------------------------------
+barramenu = Menu(raiz)
 raiz.config(menu=barramenu)
 
-archivomenu=Menu(barramenu, tearoff=0)
-archivoEdicion=Menu(barramenu, tearoff=0)#El tear siginifca lagriga y tearoff es para quitar como la linea punteada del submenu
-archivotools=Menu(barramenu,tearoff=0)
-archivoayuda=Menu(barramenu,tearoff=0)
+archivomenu = Menu(barramenu, tearoff=0)
+# El tear siginifca lagriga y tearoff es para quitar como la linea punteada del submenu
+archivoEdicion = Menu(barramenu, tearoff=0)
+archivotools = Menu(barramenu, tearoff=0)
+archivoayuda = Menu(barramenu, tearoff=0)
 
-#Para que se muestre el menu
+# Para que se muestre el menu
 barramenu.add_cascade(label="Archivo", menu=archivomenu)
 barramenu.add_cascade(label="Editar", menu=archivoEdicion)
 barramenu.add_cascade(label="Herramientas", menu=archivotools)
@@ -54,26 +55,36 @@ barramenu.add_cascade(label="Ayuda", menu=archivoayuda)
 
 #-----------------------------------------Sub opciones ó sub menus del menu principal----------------------------------------------------#
 
-#----------Submenus de archivo menu
-archivomenu.add_command(label="Nuevo archivo" )                                                                                         #
-archivomenu.add_command(label="Abrir Archivo", command=Abrir )                                                                                           #
-archivomenu.add_command(label="Guardar", command=guardar)                                                                                                 #
-archivomenu.add_command(label="Guardar como", command=guardar_como)                                                                                            #
-archivomenu.add_separator()#Sirve para que aparezca una linea para separar en bloques el submenu de un menu en este caso de archivomenu  #
-archivomenu.add_command(label="Cerrar")                                                                                                  #
-archivomenu.add_command(label="Exit" )                                                                                                    #
+# ----------Submenus de archivo menu
+#
+archivomenu.add_command(label="Nuevo archivo")
+#
+archivomenu.add_command(label="Abrir Archivo", command=Abrir)
+#
+archivomenu.add_command(label="Guardar", command=guardar)
+#
+archivomenu.add_command(label="Guardar como", command=guardar_como)
+# Sirve para que aparezca una linea para separar en bloques el submenu de un menu en este caso de archivomenu  #
+archivomenu.add_separator()
+#
+archivomenu.add_command(label="Cerrar")
+#
+archivomenu.add_command(label="Exit")
 
 #-----submenus de archivo Edicion -----------------------                                                                                #
-archivoEdicion.add_command(label="Cortar" )                                                                                               #
-archivoayuda.add_command(label="Buscar actualizaciones")                                                                                 #
-archivoEdicion.add_command(label="Pegar")                                                                                                #
+#
+archivoEdicion.add_command(label="Cortar")
+#
+archivoayuda.add_command(label="Buscar actualizaciones")
+#
+archivoEdicion.add_command(label="Pegar")
 
 
 #-------------submenus de archivo ayuda------------------                                                                                #
-archivoayuda.add_command(label="Version")                                                                                                #
-archivoayuda.add_command(label="Acerca de")                                                                                              #
-
-
+#
+archivoayuda.add_command(label="Version")
+#
+archivoayuda.add_command(label="Acerca de")
 
 
 raiz.mainloop()
